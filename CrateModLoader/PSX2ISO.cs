3757e19c2308afce6ce5cc175e5c3b1fd80cd479
ManDude <mandude@users.noreply.github.com>
2019-12-14 18:13:13 +0000

    Add support for saving PS1 BINs

--- CrateModLoader/PSX2ISO.cs
+++ CrateModLoader/PSX2ISO.cs
@@ -16,4 +16,51 @@ namespace CrateModLoader
             }
         }
     }
+
+    public static class ISO2PSX
+    {
+        public static void Run(Stream i, Stream o)
+        {
+            byte[] buffer = new byte[2352];
+            buffer[0] = 0;
+            buffer[1] = 0xFF;
+            buffer[2] = 0xFF;
+            buffer[3] = 0xFF;
+            buffer[4] = 0xFF;
+            buffer[5] = 0xFF;
+            buffer[6] = 0xFF;
+            buffer[7] = 0xFF;
+            buffer[8] = 0xFF;
+            buffer[9] = 0xFF;
+            buffer[10] = 0xFF;
+            buffer[11] = 0;
+            buffer[15] = 2;
+            int minutes = 0;
+            int seconds = 2;
+            int frames = 0;
+            while (true)
+            {
+                buffer[12] = (byte)((minutes / 10 * 16) | (minutes % 10));
+                buffer[13] = (byte)((seconds / 10 * 16) | (seconds % 10));
+                buffer[14] = (byte)((frames / 10 * 16) | (frames % 10));
+                int length = i.Read(buffer, 24, 2048);
+                if (length == 0)
+                    break;
+                if (length < 2048)
+                    throw new System.ApplicationException();
+                frames++;
+                if (frames == 75)
+                {
+                    seconds++;
+                    frames = 0;
+                }
+                if (seconds == 60)
+                {
+                    minutes++;
+                    seconds = 0;
+                }
+                o.Write(buffer, 0, 2352);
+            }
+        }
+    }
 }
