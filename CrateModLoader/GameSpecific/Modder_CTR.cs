e0f3bb5c7e12e0139caba7e8e3bb9ace0ae6f4ce
BetaM <foreverbetam@gmail.com>
2019-12-15 01:16:23 +0100

    Added CTR Framework, moved to VS 2017

--- CrateModLoader/GameSpecific/Modder_CTR.cs
+++ CrateModLoader/GameSpecific/Modder_CTR.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CTRFramework;
 //CTR API by DCxDemo
 
 namespace CrateModLoader
@@ -13,5 +14,32 @@ namespace CrateModLoader
         public string apiCredit = "API by DCxDemo";
         public System.Drawing.Image gameIcon = null;
         public string[] modOptions = { "No options available" };
+
+        public enum CTR_Options
+        {
+            RandomizeSomething = 0,
+        }
+
+        public void OptionChanged(int option, bool value)
+        {
+            // TODO
+        }
+
+        public void StartModProcess()
+        {
+            // TODO
+            ModProcess();
+        }
+
+        public void ModProcess()
+        {
+            // TODO
+            EndModProcess();
+        }
+
+        public void EndModProcess()
+        {
+            // TODO
+        }
     }
 }
