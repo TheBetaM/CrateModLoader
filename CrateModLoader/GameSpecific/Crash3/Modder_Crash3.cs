b6269f02605c2dc500ceea9168c751f6b653b4db
ManDude <7569514+ManDude@users.noreply.github.com>
2019-12-31 06:46:07 +0000

    Crash 3: Add mode - Randomize sound effects

--- CrateModLoader/GameSpecific/Crash3/Modder_Crash3.cs
+++ CrateModLoader/GameSpecific/Crash3/Modder_Crash3.cs
@@ -1,19 +1,32 @@
-﻿//Crash 3 API by chekwob and ManDude
+﻿using Crash;
+using System;
+using System.Collections.Generic;
+using System.IO;
+//Crash 3 API by chekwob and ManDude
 
 namespace CrateModLoader
 {
     sealed class Modder_Crash3
     {
-        public string[] modOptions = { "No options available" };
+        public string[] modOptions = {
+            "Randomize sound effects"
+        };
+
+        public bool RandomizeADIO = false;
 
         public enum ModOptions
         {
-            RandomizeSomething = 0,
+            RandomizeADIO = 0
         }
 
         public void OptionChanged(int option, bool value)
         {
-            // TODO
+            switch ((ModOptions)option)
+            {
+                case ModOptions.RandomizeADIO:
+                    RandomizeADIO = value;
+                    break;
+            }
         }
 
         public void UpdateModOptions()
@@ -23,19 +36,155 @@ namespace CrateModLoader
 
         public void StartModProcess()
         {
-            // TODO
+            // there is nothing for us to do here...
+
             ModProcess();
+
+            EndModProcess();
         }
 
-        public void ModProcess()
+        private void ModProcess()
         {
-            // TODO
-            EndModProcess();
+            Random rand = new Random(Program.ModProgram.randoSeed);
+
+            List<FileInfo> nsfs = new List<FileInfo>();
+            List<FileInfo> nsds = new List<FileInfo>();
+            DirectoryInfo di = new DirectoryInfo(Program.ModProgram.extractedPath);
+            AppendFileInfoDir(nsfs, nsds, di); // this should return all NSF/NSD file pairs
+
+            ErrorManager.EnterSkipRegion();
+
+            for (int i = 0; i < Math.Min(nsfs.Count, nsds.Count); ++i)
+            {
+                FileInfo nsfFile = nsfs[i];
+                FileInfo nsdFile = nsds[i];
+                if (Path.GetFileNameWithoutExtension(nsfFile.Name) != Path.GetFileNameWithoutExtension(nsdFile.Name))
+                {
+                    //MessageBox.Show($"NSF/NSD file pair mismatch. First mismatch:\n\n{nsfFile.Name}\n{nsdFile.Name}");
+                    continue;
+                }
+                if (RandomizeADIO) Mod_RandomizeADIO(nsfFile, nsdFile, rand);
+            }
+
+            ErrorManager.ExitSkipRegion();
+        }
+
+        private void AppendFileInfoDir(IList<FileInfo> nsfpaths, IList<FileInfo> nsdpaths, DirectoryInfo di)
+        {
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                AppendFileInfoDir(nsfpaths, nsdpaths, dir);
+            }
+            foreach (FileInfo file in di.GetFiles())
+            {
+                if (file.Extension.ToUpper() == ".NSF") nsfpaths.Add(file);
+                else if (file.Extension.ToUpper() == ".NSD") nsdpaths.Add(file);
+            }
         }
 
         public void EndModProcess()
         {
-            // TODO
+            // ...or here
+        }
+
+        private void Mod_RandomizeADIO(FileInfo nsfFile, FileInfo nsdFile, Random rand)
+        {
+            NSF nsf;
+            NewNSD nsd;
+            try
+            {
+                nsf = NSF.LoadAndProcess(File.ReadAllBytes(nsfFile.FullName), GameVersion.Crash3);
+                nsd = NewNSD.Load(File.ReadAllBytes(nsdFile.FullName));
+            }
+            catch (LoadAbortedException)
+            {
+                return;
+            }
+
+            // edit NSF
+            foreach (Chunk chunk in nsf.Chunks)
+            {
+                if (chunk is SoundChunk soundchunk)
+                {
+                    List<int> oldeids = new List<int>();
+                    foreach (Entry entry in soundchunk.Entries)
+                    {
+                        oldeids.Add(entry.EID);
+                    }
+                    foreach (Entry entry in soundchunk.Entries)
+                    {
+                        if (entry is SoundEntry soundentry)
+                        {
+                            int eid = oldeids[rand.Next(oldeids.Count)];
+                            entry.EID = eid;
+                            oldeids.Remove(eid);
+                        }
+                    }
+                }
+            }
+
+            // edit NSD
+            nsd.ChunkCount = nsf.Chunks.Count;
+            var indexdata = nsf.MakeNSDIndex();
+            nsd.HashKeyMap = indexdata.Item1;
+            nsd.Index = indexdata.Item2;
+
+            // patch object entity count
+            nsd.EntityCount = 0;
+            foreach (Chunk chunk in nsf.Chunks)
+            {
+                if (!(chunk is EntryChunk))
+                    continue;
+                foreach (Entry entry in ((EntryChunk)chunk).Entries)
+                {
+                    if (entry is NewZoneEntry zone)
+                        foreach (Entity ent in zone.Entities)
+                            if (ent.ID != null)
+                                ++nsd.EntityCount;
+                }
+            }
+
+            // fix loadlists
+            int[] eids = new int[nsd.Index.Count];
+            for (int i = 0; i < eids.Length; ++i)
+                eids[i] = nsd.Index[i].EntryID;
+            foreach (Chunk chunk in nsf.Chunks)
+            {
+                if (!(chunk is EntryChunk))
+                    continue;
+                foreach (Entry entry in ((EntryChunk)chunk).Entries)
+                {
+                    if (entry is NewZoneEntry zone)
+                    {
+                        foreach (Entity ent in zone.Entities)
+                        {
+                            if (ent.LoadListA != null)
+                            {
+                                foreach (EntityPropertyRow<int> row in ent.LoadListA.Rows)
+                                {
+                                    List<int> values = (List<int>)row.Values;
+                                    values.Sort(delegate (int a, int b) {
+                                        return Array.IndexOf(eids, a) - Array.IndexOf(eids, b);
+                                    });
+                                }
+                            }
+                            if (ent.LoadListB != null)
+                            {
+                                foreach (EntityPropertyRow<int> row in ent.LoadListB.Rows)
+                                {
+                                    List<int> values = (List<int>)row.Values;
+                                    values.Sort(delegate (int a, int b) {
+                                        return Array.IndexOf(eids, a) - Array.IndexOf(eids, b);
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            File.WriteAllBytes(nsfFile.FullName, nsf.Save());
+            File.WriteAllBytes(nsdFile.FullName, nsd.Save());
         }
     }
 }
