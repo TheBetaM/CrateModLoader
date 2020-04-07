8e6325191d58686ea2638351d93ba1652f02e73d
BetaM <foreverbetam@gmail.com>
2020-04-07 19:09:24 +0200

    Mod Crates: Fixed unsupported games not recognizing some mods

--- CrateModLoader/ModCrates.cs
+++ CrateModLoader/ModCrates.cs
@@ -93,7 +93,7 @@ namespace CrateModLoader
             {
                 for (int i = 0; i < ModList.Count; i++)
                 {
-                    if (ModList[i].TargetGame == UnsupportedGameShortName)
+                    if (ModList[i].TargetGame == UnsupportedGameShortName || ModList[i].TargetGame == AllGamesShortName)
                     {
                         SupportedMods.Add(ModList[i]);
                     }
