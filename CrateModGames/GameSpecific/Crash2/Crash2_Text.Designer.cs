1e069f3257685974a040a246dda53cfcac7bafaf
ManDude <7569514+ManDude@users.noreply.github.com>
2020-12-22 19:48:02 +0000

    Rand_ObjColDesc: Fix typo

--- CrateModGames/GameSpecific/Crash2/Crash2_Text.Designer.cs
+++ CrateModGames/GameSpecific/Crash2/Crash2_Text.Designer.cs
@@ -19,7 +19,7 @@ namespace CrateModGames.GameSpecific.Crash2 {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Crash2_Text {
@@ -448,7 +448,7 @@ namespace CrateModGames.GameSpecific.Crash2 {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Colors used in objects is shuffled around..
+        ///   Looks up a localized string similar to Colors used in objects are shuffled around..
         /// </summary>
         internal static string Rand_ObjColDesc {
             get {
