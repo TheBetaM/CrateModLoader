b3262bf3ba8b92813653afe079e4b29458b1d68f
ManDude <7569514+ManDude@users.noreply.github.com>
2020-12-26 07:22:07 +0000

    CrashTri: Fix "Randomize World Textures"

--- CrateModGames/GameSpecific/Crash1/CrashTri_Common.cs
+++ CrateModGames/GameSpecific/Crash1/CrashTri_Common.cs
@@ -379,91 +379,82 @@ namespace CrateModLoader.GameSpecific
 
         public static void Mod_RandomizeWGEOTex(NSF nsf, Random rand)
         {
-            foreach (Chunk ck in nsf.Chunks)
+            foreach (SceneryEntry e in nsf.GetEntries<SceneryEntry>())
             {
-                if (ck is EntryChunk eck)
+                List<ModelTexture> tex_list = new List<ModelTexture>(e.Textures);
+                List<ModelExtendedTexture> anim_list = new List<ModelExtendedTexture>(e.AnimatedTextures);
+                e.Textures.Clear();
+                e.AnimatedTextures.Clear();
+                while (tex_list.Count > 0)
                 {
-                    foreach (Entry en in eck.Entries)
-                    {
-                        foreach (SceneryEntry e in nsf.GetEntries<SceneryEntry>())
-                        {
-                            List<ModelTexture> tex_list = new List<ModelTexture>(e.Textures);
-                            List<ModelExtendedTexture> anim_list = new List<ModelExtendedTexture>(e.AnimatedTextures);
-                            e.Textures.Clear();
-                            e.AnimatedTextures.Clear();
-                            while (tex_list.Count > 0)
-                            {
-                                var t = tex_list[rand.Next(tex_list.Count)];
-                                e.Textures.Add(t);
-                                tex_list.Remove(t);
-                            }
-                            while (anim_list.Count > 0)
-                            {
-                                var t = anim_list[rand.Next(anim_list.Count)];
-                                e.AnimatedTextures.Add(t);
-                                anim_list.Remove(t);
-                            }
-                        }
-                        foreach (NewSceneryEntry e in nsf.GetEntries<NewSceneryEntry>())
-                        {
-                            List<ModelTexture> tex_list = new List<ModelTexture>(e.Textures);
-                            List<ModelExtendedTexture> anim_list = new List<ModelExtendedTexture>(e.AnimatedTextures);
-                            e.Textures.Clear();
-                            e.AnimatedTextures.Clear();
-                            while (tex_list.Count > 0)
-                            {
-                                var t = tex_list[rand.Next(tex_list.Count)];
-                                e.Textures.Add(t);
-                                tex_list.Remove(t);
-                            }
-                            while (anim_list.Count > 0)
-                            {
-                                var t = anim_list[rand.Next(anim_list.Count)];
-                                e.AnimatedTextures.Add(t);
-                                anim_list.Remove(t);
-                            }
-                        }
-                        /* Per-poly randomization (too random!)
-                         * 
-                        foreach (SceneryEntry e in nsf.GetEntries<SceneryEntry>())
-                        {
-                            SceneryEntry e = (SceneryEntry)en;
-                            int texture_count = e.Textures.Count;
-                            int anim_count = e.AnimatedTextures.Count;
-                            for (int i = 0; i < e.Triangles.Count; i++)
-                            {
-                                SceneryTriangle tri = e.Triangles[i];
-                                short new_tex = (short)(tri.Animated ? rand.Next(anim_count) : rand.Next(texture_count));
-                                e.Triangles[i] = new SceneryTriangle(tri.VertexA, tri.VertexB, tri.VertexC, new_tex, tri.Animated);
-                            }
-                            for (int i = 0; i < e.Quads.Count; i++)
-                            {
-                                SceneryQuad quad = e.Quads[i];
-                                short new_tex = (short)(quad.Animated ? rand.Next(anim_count) : rand.Next(texture_count));
-                                e.Quads[i] = new SceneryQuad(quad.VertexA, quad.VertexB, quad.VertexC, quad.VertexD, new_tex, quad.Unknown, quad.Animated);
-                            }
-                        }
-                        foreach (NewSceneryEntry e in nsf.GetEntries<NewSceneryEntry>())
-                        {
-                            NewSceneryEntry e = (NewSceneryEntry)en;
-                            int texture_count = e.Textures.Count;
-                            int anim_count = e.AnimatedTextures.Count;
-                            for (int i = 0; i < e.Triangles.Count; i++)
-                            {
-                                SceneryTriangle tri = e.Triangles[i];
-                                short new_tex = (short)(tri.Animated ? rand.Next(anim_count) : rand.Next(texture_count));
-                                e.Triangles[i] = new SceneryTriangle(tri.VertexA, tri.VertexB, tri.VertexC, new_tex, tri.Animated);
-                            }
-                            for (int i = 0; i < e.Quads.Count; i++)
-                            {
-                                SceneryQuad quad = e.Quads[i];
-                                short new_tex = (short)(quad.Animated ? rand.Next(anim_count) : rand.Next(texture_count));
-                                e.Quads[i] = new SceneryQuad(quad.VertexA, quad.VertexB, quad.VertexC, quad.VertexD, new_tex, quad.Unknown, quad.Animated);
-                            }
-                        }*/
-                    }
+                    var t = tex_list[rand.Next(tex_list.Count)];
+                    e.Textures.Add(t);
+                    tex_list.Remove(t);
+                }
+                while (anim_list.Count > 0)
+                {
+                    var t = anim_list[rand.Next(anim_list.Count)];
+                    e.AnimatedTextures.Add(t);
+                    anim_list.Remove(t);
                 }
             }
+            foreach (NewSceneryEntry e in nsf.GetEntries<NewSceneryEntry>())
+            {
+                List<ModelTexture> tex_list = new List<ModelTexture>(e.Textures);
+                List<ModelExtendedTexture> anim_list = new List<ModelExtendedTexture>(e.AnimatedTextures);
+                e.Textures.Clear();
+                e.AnimatedTextures.Clear();
+                while (tex_list.Count > 0)
+                {
+                    var t = tex_list[rand.Next(tex_list.Count)];
+                    e.Textures.Add(t);
+                    tex_list.Remove(t);
+                }
+                while (anim_list.Count > 0)
+                {
+                    var t = anim_list[rand.Next(anim_list.Count)];
+                    e.AnimatedTextures.Add(t);
+                    anim_list.Remove(t);
+                }
+            }
+            /* Per-poly randomization (too random!)
+                * 
+            foreach (SceneryEntry e in nsf.GetEntries<SceneryEntry>())
+            {
+                SceneryEntry e = (SceneryEntry)en;
+                int texture_count = e.Textures.Count;
+                int anim_count = e.AnimatedTextures.Count;
+                for (int i = 0; i < e.Triangles.Count; i++)
+                {
+                    SceneryTriangle tri = e.Triangles[i];
+                    short new_tex = (short)(tri.Animated ? rand.Next(anim_count) : rand.Next(texture_count));
+                    e.Triangles[i] = new SceneryTriangle(tri.VertexA, tri.VertexB, tri.VertexC, new_tex, tri.Animated);
+                }
+                for (int i = 0; i < e.Quads.Count; i++)
+                {
+                    SceneryQuad quad = e.Quads[i];
+                    short new_tex = (short)(quad.Animated ? rand.Next(anim_count) : rand.Next(texture_count));
+                    e.Quads[i] = new SceneryQuad(quad.VertexA, quad.VertexB, quad.VertexC, quad.VertexD, new_tex, quad.Unknown, quad.Animated);
+                }
+            }
+            foreach (NewSceneryEntry e in nsf.GetEntries<NewSceneryEntry>())
+            {
+                NewSceneryEntry e = (NewSceneryEntry)en;
+                int texture_count = e.Textures.Count;
+                int anim_count = e.AnimatedTextures.Count;
+                for (int i = 0; i < e.Triangles.Count; i++)
+                {
+                    SceneryTriangle tri = e.Triangles[i];
+                    short new_tex = (short)(tri.Animated ? rand.Next(anim_count) : rand.Next(texture_count));
+                    e.Triangles[i] = new SceneryTriangle(tri.VertexA, tri.VertexB, tri.VertexC, new_tex, tri.Animated);
+                }
+                for (int i = 0; i < e.Quads.Count; i++)
+                {
+                    SceneryQuad quad = e.Quads[i];
+                    short new_tex = (short)(quad.Animated ? rand.Next(anim_count) : rand.Next(texture_count));
+                    e.Quads[i] = new SceneryQuad(quad.VertexA, quad.VertexB, quad.VertexC, quad.VertexD, new_tex, quad.Unknown, quad.Animated);
+                }
+            }*/
         }
 
         public static void Mod_RandomizeTGEOCol(NSF nsf, Random rand)
